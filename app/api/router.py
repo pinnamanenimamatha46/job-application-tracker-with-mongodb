fa05da0377d232a9397557a32ad0e5b751137a22
Mamatha Pinnamneni <pinnamanenimamatha46@gmail.com>
2026-07-22 12:01:27 -0700

    Clean up API structure and remove unused routes

--- app/api/router.py
+++
@@ -1,8 +0,0 @@
-from fastapi import APIRouter
-
-from app.api.routes.applications import router as applications_router
-
-
-api_router = APIRouter()
-
-api_router.include_router(applications_router)
